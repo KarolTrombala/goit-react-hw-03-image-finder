450b01694b2eb9d452c9406439442dab50e2a2bd
Karol Trombala <ktrombala@gmail.com>
2023-12-27 10:28:45 +0100

    prettier update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,19 @@
-import { Component } from 'react';
-import { SearchBar } from './SearchBar/SearchBar';
-
-
-
-
-
-
-
-
+import { Component } from 'react'
+import { SearchBar } from './SearchBar/SearchBar'
 
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    return (
+        <div
+            style={{
+                height: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: 40,
+                color: '#010101',
+            }}
+        >
+            React homework template
+        </div>
+    )
+}
