3f7f805e915da8b066fa10f190e65390e445c8e2
Karol Trombala <ktrombala@gmail.com>
2024-01-29 20:48:43 +0100

    update searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,24 @@
 import { Component } from 'react'
 import { SearchBar } from './SearchBar/SearchBar'
+import {ImageGallery} from './ImageGallery/ImageGallery'
 
-export const App = () => {
-    return (
-        <div
-            style={{
-                height: '100vh',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                fontSize: 40,
-                color: '#010101',
-            }}
-        >
-            React homework template
-        </div>
-    )
+export class App extends Component {
+    state = {
+        searchQuery: '',
+    }
+
+    handleSearch = (query) => {
+        this.setState({ searchQuery: query })
+    }
+
+    render() {
+        const { searchQuery } = this.state
+
+        return (
+            <div>
+                <SearchBar onSubmit={this.handleSearch} />
+                <ImageGallery searchQuery={searchQuery} />
+            </div>
+        )
+    }
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,3 +1,50 @@
 import { Component } from 'react'
 import css from './SearchBar.module.css'
 import PropTypes from 'prop-types'
+
+
+export const SearchForm = ({ onSubmit }) => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const query = e.target.elements.search.value
+        onSubmit(query)
+    }
+
+    return (
+        <form className={css.searchform} onSubmit={handleSubmit}>
+            <button type="submit" className={css.searchform__button}>
+                <span className={css.searchform__button-label}>Search</span>
+            </button>
+
+            <input
+                name="search"
+                className={css.searchform__input}
+                type="text"
+                autoComplete="off"
+                autoFocus
+                placeholder="Search images and photos"
+            />
+        </form>
+    )
+}
+
+SearchForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
+
+
+export const Searchbar = ({ onSubmit }) => {
+    const handleSubmit = (query) => {
+        onSubmit(query)
+    }
+
+    return (
+        <header className={css.searchbar}>
+            <SearchForm onSubmit={handleSubmit} />
+        </header>
+    )
+}
+
+Searchbar.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+}
