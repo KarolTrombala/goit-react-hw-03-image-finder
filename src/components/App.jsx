5823338242f2eb99fd6bc4529fa8659639a17e4a
Karol Trombala <ktrombala@gmail.com>
2024-01-29 22:41:12 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react'
 import { Searchbar } from './SearchBar/SearchBar'
-import {ImageGallery} from './ImageGallery/ImageGallery'
+import { ImageGallery } from './ImageGallery/ImageGallery'
 
 export class App extends Component {
     state = {
