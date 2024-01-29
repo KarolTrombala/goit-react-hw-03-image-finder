01ea81da1a714d2fccf432ec7e414921b3dac2d2
Karol Trombala <ktrombala@gmail.com>
2024-01-29 22:53:16 +0100

    spinner

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,17 @@
-import {ThreeDots} from 'react'
+import { ThreeCircles } from 'react-loader-spinner'
 import { Container } from 'components/Container/Container'
 
 export const Loader = () => {
     return (
         <Container>
-            <ThreeDots
-                height="80"
-                width="80"
-                radius="9"
-                color="#3f51b5"
-                ariaLabel="three-dots-loading"
-                wrapperStyle={{}}
-                wrapperClassName=""
+            <ThreeCircles
                 visible={true}
+                height="100"
+                width="100"
+                color="#4fa94d"
+                ariaLabel="three-circles-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
             />
         </Container>
     )
